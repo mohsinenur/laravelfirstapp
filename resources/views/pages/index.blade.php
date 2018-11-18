0743e2996f6b22ecace5f8469e0ef7d3841d37f6
mohsin e nur <Nur Mohsin>
2018-11-18 15:20:07 +0600

    adding navbar and login register button

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $title }}</h1>
-    <p>This is index page</p>
+    <div class="jumbotron text-center">
+        <h1>{{ $title }}</h1>
+        <p>This is my first laravel app. Where you can create account and login to your account.</p>
+        <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
+    </div>
 @endsection
 
